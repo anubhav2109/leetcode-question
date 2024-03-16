4230d6c17f10d83625fd8f2f7b73e25bbdbca354
anubhav2109 <121336056+anubhav2109@users.noreply.github.com>
2024-03-16 22:21:42 +0530

    Time: 8 ms (79.70%), Space: 13.5 MB (43.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         for(int i=0; i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return {i,j}; //return both index whose sum is = to target
+//                 }
+//             }
+//         }
+//         return {};//return is nothing found as such pair 
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0; i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j}; //return both index whose sum is = to target
-                }
+        int n= nums.size();
+        map<int ,int > mp;
+        for(int i=0;i<n;i++){
+            int rem= target-nums[i];
+            if(mp.find(rem)!=mp.end()){
+                return {mp[rem],i};
+            }
+            else{
+                mp[nums[i]]=i;
             }
         }
-        return {};//return is nothing found as such pair 
+        return {};
     }
 };
