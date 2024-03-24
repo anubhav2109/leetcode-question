f02146864797c4be8729691afdcda2e14691c804
anubhav2109 <121336056+anubhav2109@users.noreply.github.com>
2024-03-24 14:03:55 +0530

    Time: 3 ms (58.22%), Space: 13.9 MB (89.29%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,31 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// level order traversal//
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(!root)
+//             return {};
+        
+//         queue<TreeNode*> que;
+//         vector<int> result;
+        
+//         que.push(root);
+        
+//         while(!que.empty()) {
+//             int n = que.size();
+//             TreeNode* rightNode = NULL;
+//             while(n--) {
+//                 rightNode = que.front();
+//                 que.pop();
+                
+//                 if(rightNode->left)
+//                     que.push(rightNode->left);
+//                 if(rightNode->right)
+//                     que.push(rightNode->right);
+//             }
+//             result.push_back(rightNode->val);
+//         }
+//         return result;
+//     }
+// };
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        if(!root)
-            return {};
-        
-        queue<TreeNode*> que;
-        vector<int> result;
-        
-        que.push(root);
-        
-        while(!que.empty()) {
-            int n = que.size();
-            TreeNode* rightNode = NULL;
-            while(n--) {
-                rightNode = que.front();
-                que.pop();
-                
-                if(rightNode->left)
-                    que.push(rightNode->left);
-                if(rightNode->right)
-                    que.push(rightNode->right);
-            }
-            result.push_back(rightNode->val);
+    void preorder(TreeNode* root,int level, vector<int>& result){
+        if(root==NULL){
+            return ;   
         }
+        if(result.size()<level){
+            result.push_back(root->val);
+        }
+        preorder(root->right,level+1,result);
+        preorder(root->left,level+1,result);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        preorder(root,1,result);
         return result;
     }
 };
